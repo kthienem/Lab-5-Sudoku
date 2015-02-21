3de1d03e686725fa2d07795b81d07a18cb3083b7
kthienem <kthienem@nd.edu>
2015-02-21 15:44:34 -0500

    Update

--- Puzzle.h
+++ Puzzle.h
@@ -1,23 +1,44 @@
 #define PUZZLE_H
 #ifndef PUZZLE_H
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 template <typename T>
 class Puzzle{
   public:
     Puzzle();
+    void print();
 
   private:
     vector<vector<T>> puzzleBoard; //the board containing the numbers for the puzzle
-}
+};
 
-Puzzle::Puzzle()
+template <typename T>
+Puzzle<T>::Puzzle()
 {
+  ifstream myFile;
+  myFile.open(Sudoku-Puzzle.txt);
   vec<T> tempVec;
  
   for(int i = 0; i < 9; i++){
-    for(int j = 0; i < 9; i++){
-      puzzleBoard.insert(puzzleBoard[i][j], 0);
+    for(int j = 0; i < 9; j++){
+      tempVec.push_back(myFile);
     }
+    puzzleBoard.push_back(tempVec);
+    tempVec.clear();
+  }
+}
+
+void Puzzle::print()
+{
+  for(int i = 0; i< 9; i++){
+    for(int j = 0; j < 9; j++){
+      cout << puzzleBoard[i][j];
+    }
+    cout << endl;
   }
 }
 
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Puzzle.h"
+
+using namespace std;
+
+int main()
+{
+  Puzzle<int>  myPuzzle;
+
+  myPuzzle.print();
+}
